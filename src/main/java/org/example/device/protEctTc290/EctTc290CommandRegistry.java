3ef84a0a6107c374f3057e9e190b444ea15363ce
Andrew Kantser <andrey.ka@igm-pribor.ru>
2025-10-12 18:00:10 +0300

    add KRDG SRDG

--- src/main/java/org/example/device/protEctTc290/EctTc290CommandRegistry.java
+++ src/main/java/org/example/device/protEctTc290/EctTc290CommandRegistry.java
@@ -19,6 +19,10 @@ public class EctTc290CommandRegistry extends DeviceCommandRegistry {
     protected void initCommands() {
         commandList.addCommand(createCrdgDirect());
         commandList.addCommand(createCrdgBroadcast());
+        commandList.addCommand(createKrdgDirect());
+        commandList.addCommand(createKrdgBroadcast());
+        commandList.addCommand(createSrdgDirect());
+        commandList.addCommand(createSrdgBroadcast());
         // Добавление других команд
     }
 
@@ -31,11 +35,47 @@ public class EctTc290CommandRegistry extends DeviceCommandRegistry {
         );
     }
 
+    private SingleCommand createKrdgDirect() {
+        return new SingleCommand(
+                "KRDG? ",
+                "KRDG? 1 - запрос температуры у первого датчика. KRDG? 10 - запрос температуры у десятого датчика",
+                this::parseKrdgDirect,
+                5000
+        );
+    }
+
+    private SingleCommand createSrdgDirect() {
+        return new SingleCommand(
+                "SRDG? ",
+                "SRDG? 1 - запрос температуры у первого датчика. SRDG? 10 - запрос температуры у десятого датчика",
+                this::parseKrdgDirect,
+                5000
+        );
+    }
+
     private SingleCommand createCrdgBroadcast() {
         return new SingleCommand(
                 "CRDG?",
-                "CRDG? - без аргументов. Опрос у всех сенсоров",
+                "CRDG? - без аргументов. Опрос у всех сенсоров (Градусы Цельсия)",
                 this::parseCrdgBroadcast,
+                5000
+        );
+    }
+
+    private SingleCommand createKrdgBroadcast() {
+        return new SingleCommand(
+                "KRDG?",
+                "KRDG? - без аргументов. Опрос у всех сенсоров (Градусы Кельвина)",
+                this::parseKrdgBroadcast,
+                5000
+        );
+    }
+
+    private SingleCommand createSrdgBroadcast() {
+        return new SingleCommand(
+                "SRDG?",
+                "SRDG? - без аргументов. Опрос у всех сенсоров",
+                this::parseSrdgBroadcast,
                 5000
         );
     }
@@ -73,6 +113,200 @@ public class EctTc290CommandRegistry extends DeviceCommandRegistry {
                 return answerValues;
             }else {
                 log.warn("Wrong POINT position  " + Arrays.toString(response));
+            }
+        }else {
+            log.warn("Wrong answer length " + response.length);
+            StringBuilder sb = new StringBuilder();
+            for (byte b : response) {
+                sb.append((char)b).append(" ");
+            }
+            log.warn(sb.toString());
+        }
+        return answerValues;
+
+    }
+
+    private AnswerValues parseKrdgDirect(byte[] response) {//"KRDG? 1" - direct
+        AnswerValues answerValues = null;
+        log.info("Proceed KRDG direct");
+        String example = "35629.9";
+        if(response.length >= 27 ){
+
+            if(isCorrectNumber(response)) {
+                Double value;
+                StringBuilder sb = new StringBuilder();
+                for (byte b : response) {
+                    sb.append((char)b);
+                }
+                String rsp = sb.toString();
+                rsp = rsp.trim();
+                rsp = rsp.replaceAll("[^0-9.,-]", ""); // удалится все кроме цифр и указанных знаков
+                boolean success = false;
+                try{
+                    value = (double) Double.parseDouble(rsp);
+                    success = true;
+                }catch (NumberFormatException e){
+                    log.warn("Exception " + e.getMessage());
+                    value = 0.0;
+                    success = false;
+                }
+                if(success){
+                    answerValues = new AnswerValues(1);
+                    answerValues.addValue(value, " °K");
+                }else{
+                    answerValues = null;
+                }
+                return answerValues;
+            }else {
+                log.warn("Wrong POINT position  " + Arrays.toString(response));
+            }
+        }else {
+            log.warn("Wrong answer length " + response.length);
+            StringBuilder sb = new StringBuilder();
+            for (byte b : response) {
+                sb.append((char)b).append(" ");
+            }
+            log.warn(sb.toString());
+        }
+        return answerValues;
+
+    }
+
+    private AnswerValues parseSrdgDirect(byte[] response) {//"SRDG? 1" - direct
+        AnswerValues answerValues = null;
+        log.info("Proceed SRDG direct");
+
+        if(response.length >= 37 ){
+
+            if(isCorrectNumber(response)) {
+                Double value;
+                StringBuilder sb = new StringBuilder();
+                for (byte b : response) {
+                    sb.append((char)b);
+                }
+                String rsp = sb.toString();
+                rsp = rsp.trim();
+                rsp = rsp.replaceAll("[^0-9.,-]", ""); // удалится все кроме цифр и указанных знаков
+                boolean success = false;
+                try{
+                    value = (double) Double.parseDouble(rsp);
+                    success = true;
+                }catch (NumberFormatException e){
+                    log.warn("Exception " + e.getMessage());
+                    value = 0.0;
+                    success = false;
+                }
+                if(success){
+                    answerValues = new AnswerValues(1);
+                    answerValues.addValue(value, " Units");
+                }else{
+                    answerValues = null;
+                }
+                return answerValues;
+            }else {
+                log.warn("Wrong POINT position  " + Arrays.toString(response));
+            }
+        }else {
+            log.warn("Wrong answer length " + response.length);
+            StringBuilder sb = new StringBuilder();
+            for (byte b : response) {
+                sb.append((char)b).append(" ");
+            }
+            log.warn(sb.toString());
+        }
+        return answerValues;
+
+    }
+
+    private AnswerValues parseKrdgBroadcast(byte[] response) {//"KRDG?" - broadcast
+        AnswerValues answerValues = null;
+        log.info("Proceed KRDG broadcast");
+        answerValues = null;
+        if(response.length >= 170 ){
+            StringBuilder sb = new StringBuilder();
+            for (byte b : response) {
+                sb.append((char)b);
+            }
+            String rsp = sb.toString();
+
+
+            rsp = rsp.trim();
+            String [] strValues = rsp.split(",");
+            answerValues = new AnswerValues(10);
+            for (int i = 0; i < strValues.length; i++) {
+                double value = 0.0;
+                strValues[i] = strValues[i].replace(",", "");
+                strValues[i] = strValues[i].trim();
+                strValues[i] = strValues[i].replaceAll("[^0-9.,-]", ""); // удалится все кроме цифр и указанных знаков
+                boolean success = false;
+                try{
+                    success = true;
+                    value = Double.parseDouble(strValues[i]);
+                }catch (NumberFormatException e){
+                    success = false;
+                    log.warn("Exception " + e.getMessage());
+                    //Past cleaner here
+                    //Throw exception
+
+
+                }
+                if(success){
+                    answerValues.addValue(value, " °K");
+                }else{
+                    //throw new ParseException("Exception message", "Exception message");
+                    answerValues = null;
+                }
+            }
+        }else {
+            log.warn("Wrong answer length " + response.length);
+            StringBuilder sb = new StringBuilder();
+            for (byte b : response) {
+                sb.append((char)b).append(" ");
+            }
+            log.warn(sb.toString());
+        }
+        return answerValues;
+
+    }
+
+    private AnswerValues parseSrdgBroadcast(byte[] response) {//"SRDG?" - broadcast
+        AnswerValues answerValues = null;
+        log.info("Proceed SRDG broadcast");
+        answerValues = null;
+        if(response.length >= 200 ){
+            StringBuilder sb = new StringBuilder();
+            for (byte b : response) {
+                sb.append((char)b);
+            }
+            String rsp = sb.toString();
+
+
+            rsp = rsp.trim();
+            String [] strValues = rsp.split(",");
+            answerValues = new AnswerValues(10);
+            for (int i = 0; i < strValues.length; i++) {
+                double value = 0.0;
+                strValues[i] = strValues[i].replace(",", "");
+                strValues[i] = strValues[i].trim();
+                strValues[i] = strValues[i].replaceAll("[^0-9.,-]", ""); // удалится все кроме цифр и указанных знаков
+                boolean success = false;
+                try{
+                    success = true;
+                    value = Double.parseDouble(strValues[i]);
+                }catch (NumberFormatException e){
+                    success = false;
+                    log.warn("Exception " + e.getMessage());
+                    //Past cleaner here
+                    //Throw exception
+
+
+                }
+                if(success){
+                    answerValues.addValue(value, " Units");
+                }else{
+                    //throw new ParseException("Exception message", "Exception message");
+                    answerValues = null;
+                }
             }
         }else {
             log.warn("Wrong answer length " + response.length);
